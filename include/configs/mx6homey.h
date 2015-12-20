24ce78f2758f00511970ba18ac18d19db3fa6cc8
Jeroen Vollenbrock <jeroen@vollenbrock.nl>
2015-12-20 12:54:47 +0000

    Add nulldev support

--- include/configs/mx6homey.h
+++ include/configs/mx6homey.h
@@ -53,9 +53,11 @@
 #define CONFIG_USB_MAX_CONTROLLER_COUNT	2
 #define CONFIG_USB_KEYBOARD
 #define CONFIG_SYS_USB_EVENT_POLL
+#define CONFIG_SYS_DEVICE_NULLDEV
+#define CONFIG_SILENT_CONSOLE
 #define CONFIG_PREBOOT \
-    "setenv stdin  none; " \
-    "setenv stdout none; " \
+    "setenv stdin  nulldev; " \
+    "setenv stdout nulldev; " \
     "setenv stderr serial; "
 
 /* Command definition */
